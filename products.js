376e794a9e40e2eefaed91c964ca8c8005d50258
Pinny Geliebter <pgibralter@gmail.com>
2021-07-14 15:06:44 -0400

    adds loadProduct method that uses api call and adds button to call loadProduct

--- products.js
+++ products.js
@@ -7,4 +7,12 @@ var app = new Vue({
       products: "HEYYYYYOOOO",
     };
   },
+  methods: {
+    loadProducts: function () {
+      axios.get("http://localhost:3000/products").then((response) => {
+        console.log(response.data);
+        this.products = response.data;
+      });
+    },
+  },
 });
